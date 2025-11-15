6bc8dc11990a23eefd9e697f2f5711809b4532b9
Anvesh Ramelli <anveshramelli1999@gmail.com>
2025-11-15 16:07:17 +0530

    added logo

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,6 +1,7 @@
 const Topbar = () => {
   return (
-    <header className="w-full bg-white shadow-sm h-14 flex items-center px-4 justify-between">
+    <header className="w-full bg-white shadow-sm h-14 flex items-center px-4 gap-2">
+      <img src="/logo.png" alt="Logo"/>
       <h1 className="text-xl font-semibold text-primary">Curiflow</h1>
     </header>
   );
